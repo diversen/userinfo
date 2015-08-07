b710e8201974764a9efa58f546aab16cb1103a8a
Dennis Iversen <dennis.iversen@gmail.com>
2015-08-07 18:37:58 +0200

    refactor lang

---
+++ lang/en/language.php
@@ -0,0 +1,34 @@
+<?php
+
+$LANG = array();
+
+// Translation of file modules/userinfo/module.php
+
+$LANG['You are logged in using email: '] = 'You are logged in using email: ';
+$LANG['Your screenname is not set yet'] = 'Your screenname is not set yet';
+$LANG['Your screenname is: '] = 'Your screenname is: ';
+$LANG['Logout'] = 'Logout';
+$LANG['Submitted by: '] = 'Submitted by: ';
+$LANG['User'] = 'User';
+$LANG['Profile page '] = 'Profile page ';
+$LANG['This is your profile.'] = 'This is your profile.';
+$LANG['No bio yet'] = 'No bio yet';
+$LANG['Your profile'] = 'Your profile';
+$LANG['Your profile is empty!'] = 'Your profile is empty!';
+$LANG['Screen name'] = 'Screen name';
+$LANG['Website'] = 'Website';
+$LANG['No website'] = 'No website';
+$LANG['Unknown'] = 'Unknown';
+$LANG['Age'] = 'Age';
+$LANG['Your profile has been updated'] = 'Your profile has been updated';
+$LANG['Not a valid website'] = 'Not a valid website';
+$LANG['Not a valid birthday'] = 'Not a valid birthday';
+$LANG['You are not that old'] = 'You are not that old';
+$LANG['Edit your profile information'] = 'Edit your profile information';
+$LANG['Your website'] = 'Your website';
+$LANG['Your birthday. Format is yyyy-mm-dd'] = 'Your birthday. Format is yyyy-mm-dd';
+$LANG['Write a few words about yourself'] = 'Write a few words about yourself';
+$LANG['Update'] = 'Update';
+// Translation of file modules/userinfo/install.inc
+
+$LANG['Profile'] = 'Profile';
