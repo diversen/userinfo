177fb345822d245af6cdfb32b354a41cf3d5c3a4
Dennis Iversen <dennis.iversen@gmail.com>
2015-10-07 14:38:50 +0200

    lang

--- lang/en/language.php
+++ lang/en/language.php
@@ -16,16 +16,12 @@ $LANG['Profile page '] = 'Profile page ';
 $LANG['No bio yet'] = 'No bio yet';
 $LANG['Your profile'] = 'Your profile';
 $LANG['Your profile is empty!'] = 'Your profile is empty!';
-$LANG['Screen name'] = 'Screen name';
-$LANG['Website'] = 'Website';
-$LANG['No website'] = 'No website';
-$LANG['Unknown'] = 'Unknown';
-$LANG['Age'] = 'Age';
 $LANG['Your profile has been updated'] = 'Your profile has been updated';
 $LANG['Not a valid website'] = 'Not a valid website';
 $LANG['Not a valid birthday'] = 'Not a valid birthday';
 $LANG['You are not that old'] = 'You are not that old';
 $LANG['Edit your profile information'] = 'Edit your profile information';
+$LANG['Screen name'] = 'Screen name';
 $LANG['Your website'] = 'Your website';
 $LANG['Your birthday. Format is yyyy-mm-dd'] = 'Your birthday. Format is yyyy-mm-dd';
 $LANG['Write a few words about yourself'] = 'Write a few words about yourself';
@@ -34,6 +30,13 @@ $LANG['Anonymous user'] = 'Anonymous user';
 // Translation of file modules/userinfo/install.inc
 
 $LANG['Profile'] = 'Profile';
+// Translation of file modules/userinfo/views.php
+
+$LANG['Screen name'] = 'Screen name';
+$LANG['Website'] = 'Website';
+$LANG['No website'] = 'No website';
+$LANG['Unknown'] = 'Unknown';
+$LANG['Age'] = 'Age';
 // Translation of file modules/userinfo/menu.inc
 
 $LANG['Your profile'] = 'Your profile';
