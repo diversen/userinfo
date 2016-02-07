3d26f17612f561e379f7582bec5bdf5d39345004
Dennis Iversen <dennis.iversen@gmail.com>
2016-02-07 18:45:20 +0100

    lang

--- lang/da/language.php
+++ lang/da/language.php
@@ -2,14 +2,6 @@
 
 $LANG = array();
 
-$LANG['Profile'] = 'Profil';
-$LANG['Screen name'] = 'Skærmnavn';
-$LANG['Website'] = 'Websted';
-$LANG['No website'] = 'Ingen hjemmeside';
-$LANG['Unknown'] = 'Ukendt';
-$LANG['Age'] = 'Alder';
-$LANG['Your profile'] = 'Din profil';
-$LANG['Edit your profile'] = 'Rediger din profil';
 $LANG['You are logged in using email: '] = 'Du er logget ind via e-mail:';
 $LANG['Your screenname is not set yet'] = 'Din ScreenName er ikke indstillet endnu';
 $LANG['Your screenname is: '] = 'Dit ScreenName er:';
@@ -20,13 +12,21 @@ $LANG['User profile'] = 'Brugerprofil';
 $LANG['User'] = 'Bruger';
 $LANG['Profile page '] = 'Profilside';
 $LANG['No bio yet'] = 'Nr bio endnu';
+$LANG['Your profile'] = 'Din profil';
 $LANG['Your profile is empty!'] = 'Din profil er tom!';
 $LANG['Your profile has been updated'] = 'Din profil er blevet opdateret';
 $LANG['Not a valid website'] = 'Ikke et gyldigt hjemmeside';
 $LANG['Not a valid birthday'] = 'Ikke et gyldigt fødselsdag';
 $LANG['You are not that old'] = 'Du er ikke så gammel';
 $LANG['Edit your profile information'] = 'Rediger dine profiloplysninger';
+$LANG['Screen name'] = 'Skærmnavn';
 $LANG['Your website'] = 'Din hjemmeside';
 $LANG['Your birthday. Format is yyyy-mm-dd'] = 'Din fødselsdag. Formatet er åååå-mm-dd';
 $LANG['Write a few words about yourself. You may use markdown.'] = 'Skriv et par ord om dig selv. Du må bruge markdown.';
 $LANG['Update'] = 'Opdatering';
+$LANG['Profile'] = 'Profil';
+$LANG['Website'] = 'Websted';
+$LANG['No website'] = 'Ingen hjemmeside';
+$LANG['Unknown'] = 'Ukendt';
+$LANG['Age'] = 'Alder';
+$LANG['Edit your profile'] = 'Rediger din profil';
