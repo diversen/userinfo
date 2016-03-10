bba3062fd400e8cdf39555028bc248e75482ef4a
Dennis Iversen <dennis.iversen@gmail.com>
2016-03-10 15:46:05 +0100

    lang

--- lang/da/language.php
+++ lang/da/language.php
@@ -2,13 +2,8 @@
 
 $LANG = array();
 
-$LANG['Profile'] = 'Profil';
-$LANG['Screen name'] = 'Skærmnavn';
-$LANG['Website'] = 'Websted';
-$LANG['No website'] = 'Ingen hjemmeside';
-$LANG['Unknown'] = 'Ukendt';
-$LANG['Age'] = 'Alder';
 $LANG['Usage'] = 'Forbrug';
+$LANG['Profile'] = 'Profil';
 $LANG['Your profile'] = 'Din profil';
 $LANG['Edit your profile'] = 'Rediger din profil';
 $LANG['Submitted by: '] = 'Indsendt af:';
@@ -27,7 +22,12 @@ $LANG['Not a valid website'] = 'Ikke et gyldigt hjemmeside';
 $LANG['Not a valid birthday'] = 'Ikke et gyldigt fødselsdag';
 $LANG['You are not that old'] = 'Du er ikke så gammel';
 $LANG['Edit your profile information'] = 'Rediger dine profiloplysninger';
+$LANG['Screen name'] = 'Skærmnavn';
 $LANG['Your website'] = 'Din hjemmeside';
 $LANG['Your birthday. Format is yyyy-mm-dd'] = 'Din fødselsdag. Formatet er åååå-mm-dd';
 $LANG['Write a few words about yourself. You may use markdown.'] = 'Skriv et par ord om dig selv. Du må bruge markdown.';
 $LANG['Update'] = 'Opdatering';
+$LANG['Website'] = 'Websted';
+$LANG['No website'] = 'Ingen hjemmeside';
+$LANG['Unknown'] = 'Ukendt';
+$LANG['Age'] = 'Alder';
